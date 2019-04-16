3eec83e417886f58227b5f0c1b163576fa0d1b70
Fajar Ilham Alqodri <48490920+ilhamalqodri@users.noreply.github.com>
2019-04-16 11:58:43 +0700

    Add files via upload

---
+++ Karyawan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2340
+{
+    class Karyawan
+    {
+        private string nik;
+        private string nama;
+        private int gaji;
+        private int bonus;
+
+        public string Nik
+        {
+            get { return nik; }
+            set { nik = value; }
+        }
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+        public int GajiBulanan
+        {
+            get { return gaji; }
+            set { gaji = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2340
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan ky0 = new Karyawan();
+            ky0.Nik = "190302123";
+            ky0.Nama = "Deku";
+            ky0.GajiBulanan = 3000000;
+
+            Karyawan ky1 = new Karyawan();
+            ky1.Nik = "190302124";
+            ky1.Nama = "Kacchan";
+            ky1.GajiBulanan = 2000000;
+
+            if (ky0.GajiBulanan < 0)
+                ky0.GajiBulanan = 0;
+            if (ky1.GajiBulanan < 0)
+                ky1.GajiBulanan = 0;
+
+            Console.WriteLine("No  Nik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("_________________________________________");
+            Console.WriteLine("1. {0} {1}\t {2}", ky0.Nik, ky0.Nama, ky0.GajiBulanan);
+            Console.WriteLine("1. {0} {1}\t {2}", ky1.Nik, ky1.Nama, ky1.GajiBulanan);
+
+            Console.WriteLine("");
+            Console.WriteLine("\nAnjayyyyy kenaikan gaji 10%!!");
+            Console.WriteLine("");
+
+            Console.WriteLine("No  Nik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("_________________________________________");
+            Console.WriteLine("1. {0} {1}\t {2}", ky0.Nik, ky0.Nama, ky0.GajiBulanan + ky0.GajiBulanan * 0.10);
+            Console.WriteLine("1. {0} {1}\t {2}", ky1.Nik, ky1.Nama, ky1.GajiBulanan + ky1.GajiBulanan * 0.10);
+            Console.ReadKey();
+        }
+    }
+}
