6b1593a103fe9e7162694b7c3e5c1d16afc98007
Fajar Ilham Alqodri <48490920+ilhamalqodri@users.noreply.github.com>
2019-05-02 03:03:47 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,25 +10,25 @@ namespace Responsi Pemrog tahun 2018/2019;
     {
         static void Main(string[] args)
         {
-            Karyawan ky0 = new Karyawan();
-            ky0.Nik = "123456";
-            ky0.Nama = "Paijo";
-            ky0.GajiBulanan = 3000000;
+            Karyawan ObjKaryawan = new Karyawan();
+            ObjKaryawan.Nik = "123456";
+            ObjKaryawan.Nama = "Paijo";
+            ObjKaryawan.GajiBulanan = 3000000;
 
-            Karyawan ky1 = new Karyawan();
-            ky1.Nik = "123456";
-            ky1.Nama = "Budi";
-            ky1.GajiBulanan = -1000;
+            Karyawan ObjKaryawan2 = new Karyawan();
+            ObjKaryawan2.Nik = "123456";
+            ObjKaryawan2.Nama = "Budi";
+            ObjKaryawan2.GajiBulanan = -1000;
 
-            if (ky0.GajiBulanan < 0)
-                ky0.GajiBulanan = 0;
-            if (ky1.GajiBulanan < 0)
-                ky1.GajiBulanan = 0;
+            if (ObjKaryawan.GajiBulanan < 0)
+                ObjKaryawan.GajiBulanan = 0;
+            if (ObjKaryawan2.GajiBulanan < 0)
+                ObjKaryawan2.GajiBulanan = 0;
 
             Console.WriteLine("No  Nik/Nama\t\tGaji Bulanan");
             Console.WriteLine("_________________________________________");
-            Console.WriteLine("1. {0} {1}\t {2}", ky0.Nik, ky0.Nama, ky0.GajiBulanan);
-            Console.WriteLine("1. {0} {1}\t {2}", ky1.Nik, ky1.Nama, ky1.GajiBulanan);
+            Console.WriteLine("1. {0} {1}\t {2}", ObjKaryawan.Nik, ObjKaryawan.Nama, ObjKaryawan.GajiBulanan);
+            Console.WriteLine("1. {0} {1}\t {2}", ObjKaryawan2.Nik, ObjKaryawan2.Nama, ObjKaryawan2.GajiBulanan);
 
             Console.WriteLine("");
             Console.WriteLine("\nAsyiiiiikkk kenaikan gaji 10%!!");
@@ -36,8 +36,8 @@ namespace Responsi Pemrog tahun 2018/2019;
 
             Console.WriteLine("No  Nik/Nama\t\tGaji Bulanan");
             Console.WriteLine("_________________________________________");
-            Console.WriteLine("1. {0} {1}\t {2}", ky0.Nik, ky0.Nama, ky0.GajiBulanan + ky0.GajiBulanan * 0.10);
-            Console.WriteLine("1. {0} {1}\t {2}", ky1.Nik, ky1.Nama, ky1.GajiBulanan + ky1.GajiBulanan * 0.10);
+            Console.WriteLine("1. {0} {1}\t {2}", ObjKaryawan.Nik, ObjKaryawan.Nama, ObjKaryawan.GajiBulanan + ObjKaryawan.GajiBulanan * 0.10);
+            Console.WriteLine("1. {0} {1}\t {2}", ObjKaryawan2.Nik, ObjKaryawan2.Nama, ObjKaryawan2.GajiBulanan + ObjKaryawan2.GajiBulanan * 0.10);
             Console.ReadKey();
         }
     }
